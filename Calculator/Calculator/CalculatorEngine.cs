13b368d61975f118e9e1acd816913913d1cdf696
alekwtz <alekwtzz@gmail.com>
2017-01-14 11:27:00 +0200

    First Commit

---
+++ Calculator/Calculator/CalculatorEngine.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace Calculator
+{
+    class CalculatorEngine
+    {
+        private double result;
+        private double input;
+
+        public void Input(double number)
+        {
+            input = number;
+        }
+
+        public void Input(string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    result += input;
+                    break;
+                case "-":
+                    result -= input;
+                    break;
+                case "*":
+                    result *= input;
+                    break;
+                case "/":
+                    result /= input;
+                    break;
+                case "sin(x)":
+                    result = Math.Sin(result);
+                    break;
+            }
+        }
+
+        public double GetResult()
+        {
+
+            return result;
+
+        }
+    }
+}
---
+++ Calculator/Calculator/CalculatorForm.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private CalculatorEngine calculator=new CalculatorEngine();
+
+
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void binaryOperationButton_Click(object sender, System.EventArgs e)
+        {
+            double number = double.Parse(inputTextBox.Text);
+            Button operationButton = (Button) sender;
+            string operation = operationButton.Text;
+            
+            calculator.Input(number);
+            calculator.Input(operation);
+            double result = calculator.GetResult();
+            outputTextBox.Text = result.ToString();
+            inputTextBox.Clear();
+
+        }
+
+        private void unaryOperationButton_Click(object sender, System.EventArgs e)
+        {
+            Button operationButton = (Button)sender;
+            string operation = operationButton.Text;
+
+            calculator.Input(operation);
+            double result = calculator.GetResult();
+            outputTextBox.Text = result.ToString();
+            inputTextBox.Clear();
+
+        }
+
+        
+    }
+}
